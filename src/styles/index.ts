a92318820c22f4f0e2185e4ad36e00b45c460263
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-06-21 14:29:35 -0300

    WIP: Estilizando a pagina

---
+++ src/styles/index.ts
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+    background-image: url('tesla1.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .colored:nth-child(2) {
+    background-image: url('tesla2.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .colored:nth-child(3) {
+    background-image: url('tesla3.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .colored:nth-child(4) {
+    background-image: url('tesla4.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .colored:nth-child(5) {
+    background-image: url('tesla5.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .colored:nth-child(6) {
+    background-image: url('tesla6.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .colored:nth-child(7) {
+    background-image: url('tesla7.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  @media (max-width: 600px) {
+    .colored:nth-child(1) {
+    background-image: url('tesla1mobile.jpeg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    }
+    .colored:nth-child(2) {
+      background-image: url('tesla2mobile.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+    .colored:nth-child(3) {
+      background-image: url('tesla3mobile.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+    .colored:nth-child(4) {
+      background-image: url('tesla4mobile.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+    .colored:nth-child(5) {
+      background-image: url('tesla5mobile.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+    .colored:nth-child(6) {
+      background-image: url('tesla6mobile.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+    .colored:nth-child(7) {
+      background-image: url('tesla7mobile.jpeg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+    }
+  }
+`
